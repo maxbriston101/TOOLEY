8f09da0550be3e81c227a99981d7919e405269bb
tereza dragoti <tereza.dragoti@live.it>
2023-08-22 16:35:03 +0100

    adding booking controller and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :tools
+  resources :tools do
+    resources :bookings, only: [:create]
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
